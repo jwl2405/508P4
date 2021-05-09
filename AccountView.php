58be92ae6bb402b8cb61b08f870b238ebd21b4b3
Jai <tommyli874@gmail.com>
2021-05-09 12:58:04 -0400

    Uncommented

--- AccountView.php
+++ AccountView.php
@@ -49,23 +49,18 @@
         <a href="Login.php">Log In</a>
     </div>
 
-    <table style="width:100%">
-  <tr>
-    <th>Firstname</th>
-    <th>Lastname</th>
-    <th>Age</th>
-  </tr>
-  <tr>
-    <td>Jill</td>
-    <td>Smith</td>
-    <td>50</td>
-  </tr>
-  <tr>
-    <td>Eve</td>
-    <td>Jackson</td>
-    <td>94</td>
-  </tr>
-</table>
+    <TABLE  BORDER="5">
+   <TR>
+      <TH COLSPAN="6">
+         <H3><BR>Members</H3>
+      </TH>
+   </TR>
+      <TH>First Name</TH>
+      <TH>Last Name</TH>
+      <TH>Username</TH>
+      <TH>Email Address</TH>
+      <TH>ID</TH>
+      <TH>Password</TH>
 
 </body>
 
