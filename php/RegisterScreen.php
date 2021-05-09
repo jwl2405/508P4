80a179cb686a3424d4cef084bc67ce75505a5f67
Jai <tommyli874@gmail.com>
2021-05-09 00:43:28 -0400

    Uncommented

--- php/RegisterScreen.php
+++ php/RegisterScreen.php
@@ -212,17 +212,9 @@
         <tr>
             <th>Firstname</th>
             <th>Lastname</th>
-            <th>Age</th>
-        </tr>
-        <tr>
-            <td>Jill</td>
-            <td>Smith</td>
-            <td>50</td>
-        </tr>
-        <tr>
-            <td>Eve</td>
-            <td>Jackson</td>
-            <td>94</td>
+            <th>email</th>
+            <th>Customer_ID</th>
+
         </tr>
         </table>
 
