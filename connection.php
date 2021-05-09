bea2d9560caec798d992dd0b85a614740a65c013
Jai <tommyli874@gmail.com>
2021-05-09 19:47:33 -0400

    Uncommented

--- connection.php
+++ connection.php
@@ -73,7 +73,6 @@ if (!isset($_SESSION['user_ID']))
         $queryResult = $stmt->fetch();
         $pwd_plain = htmlspecialchars($_POST["password"]);
 
-        echo $queryResult;
         // Verify password submitted by the user with the hash stored in the database
         if(!empty($queryResult) && password_verify($pwd_plain, $queryResult[1]))
         {
