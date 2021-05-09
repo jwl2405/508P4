aaefbb0bce9f94fb0d381978ae5c95cc8f20d774
Jai <tommyli874@gmail.com>
2021-05-08 23:58:19 -0400

    Uncommented

--- php/connection.php
+++ php/connection.php
@@ -33,7 +33,7 @@ session_start();
 
 
 // If the user_ID session is not set, then the user has not logged in yet
-/*
+
 if (!isset($_SESSION['user_ID']))
 {
     // If the page is receiving the email and password from the login form then verify the login data
@@ -66,8 +66,6 @@ if (!isset($_SESSION['user_ID']))
         exit();
     }
 }
-
-*/
 
 
 
