9b2ca2c530a9a6e461796e78781a41f09437ff13
Jai <tommyli874@gmail.com>
2021-05-09 19:33:09 -0400

    Uncommented

--- connection.php
+++ connection.php
@@ -18,6 +18,8 @@ try {
     echo "Connection failed: " . $e->getMessage();
 }
 
+
+/*
 session_start();
 
 
@@ -34,7 +36,7 @@ if (isset($_SESSION['user_ID']))
         $pwd_plain = htmlspecialchars($_POST["password"]);
 
         // Verify password submitted by the user with the hash stored in the database
-        //echo $queryResult[1]; 
+        echo $queryResult[1]; 
 
         if(!empty($queryResult) && password_verify($pwd_plain, $queryResult[1]))
         {
@@ -55,4 +57,42 @@ if (isset($_SESSION['user_ID']))
         exit();
     }
 }
+?>
+*/
+session_start();
+
+// If the user_ID session is not set, then the user has not logged in yet
+if (!isset($_SESSION['user_ID']))
+{
+    // If the page is receiving the email and password from the login form then verify the login data
+    if (isset($_POST['email']) && isset($_POST['password']))
+    {
+        $stmt = $conn->prepare("SELECT ID, password FROM user WHERE email=:email");
+        $stmt->bindValue(':email', $_POST['email']);
+        $stmt->execute();
+        
+        $queryResult = $stmt->fetch();
+        
+        // Verify password submitted by the user with the hash stored in the database
+        if(!empty($queryResult) && password_verify($_POST["password"], $queryResult['password']))
+        {
+            // Create session variable
+            $_SESSION['user_ID'] = $queryResult['ID'];
+            
+            // Redirect to URL 
+            header("Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]");
+        } else {
+            // Password mismatch
+            require('login.php');
+            exit();
+        }
+    }
+    else
+    {
+        // Show login page
+        require('login.php');
+        exit();
+    }
+}
+
 ?>
