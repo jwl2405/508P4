40d52200f5f9bf1d23d5e008303e5ea2db3b50e0
Jai <tommyli874@gmail.com>
2021-05-09 20:39:01 -0400

    Uncommented

--- AccountView.php
+++ AccountView.php
@@ -7,7 +7,19 @@
     <title>
         Accounts Page
     </title>
-    
+
+    <!-- JS libraries -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+
+<!-- CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"/>
+
+<!-- My JS libraries -->
+<script src="AddMembers.js"></script>
+
     <style>
         input {
             margin: 5px;
@@ -52,6 +64,19 @@
 
 
     </div>
+
+    <div class="table-responsive">
+		<table id="table-member" name="table-member" class="table table-bordered table-striped">
+			<thead>
+				<tr>
+					<th>ID</th>
+					<th>Email</th>
+					<th>Password</th>
+					<th>Username</th>
+				</tr>
+			</thead>
+		</table>
+	</div>
 
 <fieldset>
     <legend>
