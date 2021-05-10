c67a20999f61a3f9e98ba9344b0cc950b340f5c9
Jai <tommyli874@gmail.com>
2021-05-09 21:02:28 -0400

    Uncommented

--- Members.php
+++ Members.php
@@ -6,6 +6,19 @@
     <title>
         Accounts Page
     </title>
+
+
+    <!-- JS libraries -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+
+<!-- CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"/>
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"/>
+
+<!-- My JS libraries -->
+<script src="MemverImport.js"></script>
     
     <style>
         input {
@@ -65,7 +78,7 @@
 			</thead>
 		</table>
 	</div>
-    
+
 <br>
 <fieldset>
     <legend>
