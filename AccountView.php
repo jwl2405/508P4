379179b2a834e28cdd0778850e13d11f05bdefd5
Jai <tommyli874@gmail.com>
2021-05-09 21:24:48 -0400

    Uncommented

--- AccountView.php
+++ AccountView.php
@@ -1,5 +1,5 @@
 <html>
-//<?php require_once('connection.php'); ?>
+<?php require_once('connection.php'); ?>
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -8,17 +8,17 @@
         Accounts Page
     </title>
 
-    <!-- JS libraries -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+            <!-- JS libraries -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"></script>
+        <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
 
-<!-- CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"/>
-<link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"/>
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/jquery.dataTables.min.css"/>
 
-<!-- My JS libraries -->
-<script src="AccountImport.js"></script>
+        <!-- My JS libraries -->
+        <script src="AccountImport.js"></script>
 
     <style>
         input {
